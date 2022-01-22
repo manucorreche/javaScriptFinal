6055865bb74a01248cb1dc93ce909be3be4783bc
manucorreche <manuelcorreche@gmail.com>
2022-01-22 01:07:12 +0100

    primer commit

---
+++ js/app.js
@@ -0,0 +1,3 @@
+/*Pueden simular un login de usuario, que vimos como hacerlo (if), 
+pedirle una serie de datos (un bucle), hacer algunos calculos 
+(restar, sumar, calcular iva, multiplicar), y generar una salida.*/
