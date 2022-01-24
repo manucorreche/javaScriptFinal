67fa4edf2d6d2f3f3ca747536085e668a3a7f9b4
manucorreche <manuelcorreche@gmail.com>
2022-01-24 11:49:55 +0100

    html y css terminado. Hay un problema con la navbar que desaparece en cuanto se pasa el primer break point

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
 /*Pueden simular un login de usuario, que vimos como hacerlo (if), 
 pedirle una serie de datos (un bucle), hacer algunos calculos 
 (restar, sumar, calcular iva, multiplicar), y generar una salida.*/
+
+let login = prompt("Es uds usuario de Scythe Store? - Responda Si o No").toUpperCase
+
+if (login === SI) {
+    let producto = prompt("Que tipo de producto esta buscando? Indicar con la letra que hacer referencia al producto: A. Juego Base, B. Expansion Invasores de tierras lejanas, C. Expansion El auge de Fernis, D. Expansion Vientos de guerra y paz").toUpperCase}
+
+else {alert("Puede crear un usuario en la seccion de <Crear nuevo usuario>. Muchas gracias por visitar Scythe Store")}
