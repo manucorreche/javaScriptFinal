4a2d0c400c3b7f6311bb412f2457491595874c4c
manucorreche <manuelcorreche@gmail.com>
2022-01-24 17:35:04 +0100

    modifique unas comillas pero sigo con los mismo problemas

--- js/app.js
+++ js/app.js
@@ -2,29 +2,29 @@
 
 let login = prompt("Es uds usuario de Scythe Store? - Responda Si o No").toUpperCase
 
-if (login === SI) {
+if (login === "SI") {
     let producto = prompt("Que tipo de producto esta buscando? Indicar con la letra que hacer referencia al producto: A. Juego Base, B. Expansiones, C. Libro de Arte ").toUpperCase}
-        if (producto === A)
+        if (producto ===  "A")
             alert("El precio del Juego Base es de $32.000.-")
             let compraBase = prompt ("Quiere comprar Scythe: el Juego Base responda Si o No").toUpperCase
                 if (compraBase === "SI") { alert("Su compra total es de $32.000.- Gracias por su compra")}
                 else{ alert("Gracias por visitar Scythe Store")}    
 
-        if (producto === B) { let compraExpa=prompt ("Indique que expansion desea escribiendo la letra a la que hace referencia: A. Vientos de Guerra y paz, B. Invasores de tierras lejanas, C. El auge de Fernis").toUpperCase
+        if (producto === "B") { let compraExpa=prompt ("Indique que expansion desea escribiendo la letra a la que hace referencia: A. Vientos de Guerra y paz, B. Invasores de tierras lejanas, C. El auge de Fernis").toUpperCase
             switch (compraExpa) {
-                case A:
+                case "A":
                     alert("La expansion 'Vientos de Guerra y paz'tiene un precio de $12.200")
                     let compraExpaA = prompt ("Quiere comprar la expansion Vientos de Guerra y paz? Responda Si o No").toUpperCase
                     if (compraExpaA === "SI") { alert("Su compra total es de $12.200.- Gracias por su compra")}
                     else{ alert("Gracias por visitar Scythe Store")} 
                 break;
-                case B:
+                case "B":
                     alert("La expansion 'Invasores de tierras lejanas' tiene un precio de $9.000")
                     let compraExpaB = prompt ("Quiere comprar la expansion 'Invasores de tierras lejanas'? Responda Si o No").toUpperCase
                     if (compraExpaB === "SI") { alert("Su compra total es de $9.000.- Gracias por su compra")}
                     else{ alert("Gracias por visitar Scythe Store")} 
                 break;
-                case C:
+                case "C":
                     alert("La expansion 'El auge de Fernis 'tiene un precio de $15.500")
                     let compraExpaC = prompt ("Quiere comprar la expansion El auge de Fernis '? Responda Si o No").toUpperCase
                     if (compraExpaC === "SI") { alert("Su compra total es de $15.500.- Gracias por su compra")}
@@ -35,7 +35,7 @@ if (login === SI) {
                 break;
      }
         } 
-        else if (producto === C){ alert("El libro de Arte de Scythe tiene un precio de $6.000")
+        else if (producto === "C"){ alert("El libro de Arte de Scythe tiene un precio de $6.000")
         let compraExpaA = prompt ("Quiere comprar El libro de Arte de Scythe? Responda Si o No").toUpperCase
         if (compraExpaA === "SI") { alert("Su compra total es de $6.000.- Gracias por su compra")}
         else{ alert("Gracias por visitar Scythe Store")} 
